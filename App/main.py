055aa3bf13ddf10dd54167253ecc3939cf64f7dc
lizzzu <lizudim@yahoo.com>
2023-12-01 14:15:57 +0200

    added assert

--- App/main.py
+++ App/main.py
@@ -1,4 +1,3 @@
-
 from models.item import Item
 from models.zone import Zone
 from models.rfidTag import RFIDTag
@@ -7,14 +6,14 @@ from models.employee import Employee, Role
 from models.warehouse import Warehouse
 
 if __name__ == "__main__":
-    
+
     warehouse = Warehouse(145)
 
     # Create roles
-    #admin_role = Role(["add_tag", "remove_tag"])
+    # admin_role = Role(["add_tag", "remove_tag"])
     basic_role = Role(["add_item"])
 
-    # create employee 
+    # create employee
     employee = Employee(12,"employee1",basic_role)
     warehouse.add_employee(employee)
     warehouse.add_observer_to_all_readers(employee)
@@ -30,57 +29,67 @@ if __name__ == "__main__":
         reader = RFIDReader(id, id)
         zone.add_rfid_reader(reader)
 
-        warehouse.add_zone(zone)    
+        warehouse.add_zone(zone)
 
         tag_id = id*10
         for _ in range(10):
-                tag_id += 1 
-                item = Item(tag_id,f"item {id}", tag_id)
-                tag = RFIDTag(tag_id)
+            tag_id += 1
+            item = Item(tag_id, f"item {id}", tag_id)
+            tag = RFIDTag(tag_id)
 
-                warehouse.add_item(employee, item, tag)
-                warehouse.add_tag_to_zone(id, tag)
+            result = warehouse.add_item(employee, item, tag)
+            if not result:
+                print("Warehouse is at full capacity. Cannot add more items.")
+
+            result = warehouse.add_tag_to_zone(id, tag)
+            assert result, f"Zone {id} does not exist"
 
     # add tag in zone 2 - employee is notified
+    zone = 2
     tag333 = RFIDTag(333)
-    print(warehouse.add_tag_to_zone(2,tag333))
+    result = warehouse.add_tag_to_zone(zone, tag333)
+    assert result, f"Zone {zone} does not exist"
 
     # find tag in warehouse
     target_tag_id = 333
     result = warehouse.find_tag_in_warehouse(target_tag_id)
-    print(result)
-    
+    assert result, f"Tag {target_tag_id} does not exist"
+
     # print zone and their tags
     for zone in warehouse:
         print(f"Zone: {zone.get_zone_id()}")
-        #for tag in zone:
-        #    print(f"  RFID Tag ID: {tag.tag_id}")
 
         zone_tags = zone.get_items()
         print(f"  Tags in the zone: {[tag.tag_id for tag in zone_tags]}")
-    
+
     # find item in warehouse
     tag_id_to_search = 22
     result = warehouse.find_item_location(tag_id_to_search)
-    print("\nTag location: ",result)
+    assert result != -1, f"Tag {tag_id_to_search} does not exist"
+    print(f"\nTag location: {result}")
 
     # get item info
-    print("Item info: ",warehouse.get_item_info_by_id(22))
+    item = 22
+    result = warehouse.get_item_info_by_id(item)
+    assert result, f"Item {item} does not exist"
+    print(f"Item info: {result}")
 
     # get item location(zone id)
-    print("Tag location: ",warehouse.find_zone_location_of_item(22),"\n\n")
+    result = warehouse.find_zone_location_of_item(item)
+    assert result != -1, f"Item {item} does not exist"
+    print(f"Tag location: {result}\n")
 
     # move item from one zone to another
-    tag = warehouse.get_tag_by_id(61)
-    warehouse.move_item(tag,6,1)
+    tag_id_to_search = 61
+    tag = warehouse.get_tag_by_id(tag_id_to_search)
+    assert tag, f"Tag {tag_id_to_search} does not exist"
+    warehouse.move_item(tag, 6, 1)
 
     print(warehouse.get_tags_id_from_zone(1))
     print(warehouse.get_tags_id_from_zone(6))
 
     # find item in warehouse
-    print("\nFind item:", warehouse.find_item_location(401))
-
-    
-
-
-        
+    item = 100
+    result = warehouse.find_item_location(item)
+    assert result != -1, f"Item {item} does not exist"
+    print(f"\nFind item {item}: zone {result}")
--- App/models/warehouse.py
+++ App/models/warehouse.py
@@ -1,7 +1,7 @@
 from models.item import Item
 from models.zone import Zone
 from models.rfidTag import RFIDTag
-from models.employee import Employee, Role
+from models.employee import Employee
 from security import AccessControl, check_access_control
 
 class Warehouse:
@@ -18,17 +18,47 @@ class Warehouse:
             cls._instance.capacity = capacity
             cls.access_control = AccessControl()
         return cls._instance
-    
+
+    def get_capacity(self):
+        return self.capacity
+
+    def get_zones(self):
+        return self.zones
+
+    def get_items(self):
+        return self.items
+
+    def get_rfid_tags(self):
+        return self.rfid_tags
+
+    def get_tags_id_from_zone(self, zone_id: int):
+        try:
+            for zone in self.zones:
+                if zone.id == zone_id:
+                    elements = [tag.tag_id for tag in zone.rfid_reader.rfid_tags]
+                    return elements
+        except IndexError:
+            return []
+
+    def get_tag_by_id(self, tag_id: int) -> RFIDTag:
+        for tag in self.rfid_tags:
+            if tag.tag_id == tag_id:
+                return tag
+        return None
+
+    def get_item_info_by_id(self, item_tag_id: int) -> Item:
+        for item in self.items:
+            if item.rfid_tag_id == item_tag_id:
+                return item
+        return None
+
     @check_access_control("add_item")
     def add_item(self, employee: Employee, item: Item, tag: RFIDTag) -> bool:
         if len(self.items) < self.capacity:
             self.items.append(item)
             self.rfid_tags.append(tag)
             return True
-        else:
-            print(f"Warehouse is at full capacity. Cannot add more items.")
-            return False
-
+        return False
 
     def remove_item(self, item_id: int) -> bool:
         for item in self.items:
@@ -40,7 +70,7 @@ class Warehouse:
                         self.items.remove(tag)
                 return True
         return False
-    
+
     def add_zone(self, zone: Zone):
         self.zones.append(zone)
 
@@ -50,7 +80,7 @@ class Warehouse:
                 self.zones.remove(zone)
                 return True
         return False
-    
+
     def add_employee(self, employee: Employee):
         self.employee.append(employee)
 
@@ -60,25 +90,7 @@ class Warehouse:
                 self.employee.remove(employee)
                 return True
         return False
-    
-    def get_capacity(self):
-        return self.capacity
 
-    def get_zones(self):
-        return self.zones
-    
-    def get_items(self):
-        return self.items
-    
-    def get_rfid_tags(self):
-        return self.rfid_tags
-    
-    def find_tag_in_warehouse(self, target_tag_id: int ) -> bool:
-        for zone in self.zones:
-            if zone.is_tag_in_zone(target_tag_id):
-                return True
-        return False    
-    
     def add_tag_to_zone(self, zone_id: int, tag: RFIDTag) -> bool:
         for zone in self.zones:
             if zone.id == zone_id:
@@ -98,47 +110,32 @@ class Warehouse:
     def move_item(self, tag: RFIDTag, zoneFrom_id: int, zoneTo_id: int):
         self.remove_tag_from_zone(zoneFrom_id, tag.tag_id)
         self.add_tag_to_zone(zoneTo_id, tag)
-                
-    def get_tags_id_from_zone(self, zone_id: int):
-        try:
-            for zone in self.zones:
-                if zone.id == zone_id:
-                    elements = [tag.tag_id for tag in zone.rfid_reader.rfid_tags]
-                    return elements
-        except IndexError:
-            return []
+
+    def find_tag_in_warehouse(self, target_tag_id: int ) -> bool:
+        for zone in self.zones:
+            if zone.is_tag_in_zone(target_tag_id):
+                return True
+        return False
 
     def find_item_location(self, item_tag_id: int) -> int:
         for zone in self.zones:
             for tag in zone.rfid_reader.rfid_tags:
                 if tag.tag_id == item_tag_id:
-                    return zone.get_zone_id()               
+                    return zone.get_zone_id()
         return -1
-    
-    def get_tag_by_id(self, tag_id: int) -> RFIDTag:
-        for tag in self.rfid_tags:
-            if tag.tag_id == tag_id:
-                return tag
-        return None
-    
-    def get_item_info_by_id(self, item_tag_id: int) -> Item:
-        for item in self.items:
-            if item.rfid_tag_id == item_tag_id:
-                return item
-        return None
 
     def find_zone_location_of_item(self, item_tag_id: int) -> int:
-        for zone_index, zone in enumerate(self.zones):
+        for _, zone in enumerate(self.zones):
             for tag in zone.rfid_reader.rfid_tags:
                 if tag.tag_id == item_tag_id:
                     return zone.id
         return -1
-    
+
     # Iterator pattern
     def __iter__(self):
         return iter(self.zones)
-    
-    # Observer design 
+
+    # Observer design
     def add_observer_to_all_readers(self, observer: Employee):
         for zone in self.zones:
             zone.add_observer(observer)
@@ -146,4 +143,3 @@ class Warehouse:
     def remove_observer_from_zones(self, observer: Employee):
         for zone in self.zones:
             zone.remove_observer(observer)
-    
